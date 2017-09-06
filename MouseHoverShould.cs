ebf088c1b9f12614706b8b415d7d3f4a376e98cd
Thomas-Rice <tikkleanim@gmail.com>
2017-09-06 09:45:41 +0100

    init and refactored to test case

---
+++ MouseHoverShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+
+namespace mouseHover
+{
+    [TestFixture]
+    public class MouseHoverShould
+    {
+
+        [TestCase("0,0 N; M", ExpectedResult = "0,1 N")]
+        [TestCase("0,1 N; M", ExpectedResult = "0,2 N")]
+        [TestCase("1,1 N; M", ExpectedResult = "1,2 N")]
+        public string MoveForwardOneStep(string input)
+        {
+            return MovementCalculator.Move(input);
+        }
+    }
+
+    public static class MovementCalculator
+    {
+        public static string Move(string input)
+        {
+            var startingPositionX = (int)char.GetNumericValue(input[0]);
+            var startingPositionY = (int)char.GetNumericValue(input[2]);
+
+            startingPositionY += 1;
+            return $"{startingPositionX},{startingPositionY} N";
+        }
+    }
+}
